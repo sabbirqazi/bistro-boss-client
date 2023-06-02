388e8918d7f941349ca8bfb987260756014d33fa
Sabbir Ahmed <sabbirahmed.probd@gmail.com>
2023-06-02 07:30:07 +0600

    initialize firebase

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,31 +1,35 @@
-
-import {
-    createBrowserRouter,
-  } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Menu from "../pages/Menu/Menu/Menu";
 import Order from "../pages/Order/Order/Order";
 
-
-
-  export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-        {
-            path: '/',
-            element: <Home></Home>
-        },
-        { 
-        path: 'menu', 
-        element: <Menu></Menu>
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
       },
       {
-        path: 'order/:category',
-        element: <Order></Order>
-      }
-      ]
-    },
-  ]);
+        path: "menu",
+        element: <Menu></Menu>,
+      },
+
+      {
+        path: "order/:category",
+        element: <Order></Order>,
+      },
+      {
+        path: "login",
+        element: <Menu></Menu>,
+      },
+      {
+        path: "signup",
+        element: <Menu></Menu>,
+      },
+    ],
+  },
+]);
