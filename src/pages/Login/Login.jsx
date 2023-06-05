cbb0042f312aba20db0c327ad2975a0c63ac9fc5
Sabbir Ahmed <sabbirahmed.probd@gmail.com>
2023-06-06 03:37:47 +0600

    all users shown for admin

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,6 +5,7 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import Swal from 'sweetalert2/dist/sweetalert2.all.js';
 import { AuthContext } from '../../providers/AuthProvider';
+import SocialLogin from '../Shared/SocialLogin/SocialLogin';
 
 const Login = () => {
     const [disabled, setDisabled] = useState(true);
@@ -92,6 +93,7 @@ const Login = () => {
                             </div>
                         </form>
                         <p><small>New Here? <Link to="/signup">Create an account</Link> </small></p>
+                        <SocialLogin></SocialLogin>
                     </div>
                 </div>
             </div>
